25217eb2598993cc2eff412fbf1c0561a1f7c62e
Kevin Vargas Miranda <kevinandresvm2505@gmail.com>
2025-06-27 17:08:25 -0600

    Limpieza de Car

--- src/domain/Car.java
+++ src/domain/Car.java
@@ -17,109 +17,118 @@ public class Car implements Runnable {
 	private static final int VELOCITY_STANDARD = 1000;
 	private final int id;
 	private NodeV origin;
-        private NodeV destination;
-        private MainController controller;
+	private NodeV destination;
+	private MainController controller;
 
-        public Car(NodeV origin, NodeV destination, MainController controller) {
-                this.id = ++counter;
-                this.origin = origin;
-                this.destination = destination;
-                this.controller = controller;
-        }
+	public Car(NodeV origin, NodeV destination, MainController controller) {
+		this.id = ++counter;
+		this.origin = origin;
+		this.destination = destination;
+		this.controller = controller;
+	}
 
-        @Override
-        public void run() {
-                Graph g = GraphRoad.getGraph();
-                if (g == null)
-                        return;
+	@Override
+	public void run() {
+		Graph g = GraphRoad.getGraph();
 
-                while (true) {
-                        int[] path = Dijkstra.buildPath(origin.getData(), destination.getData(), g);
+		while (true) {
+			int[] path = Dijkstra.buildPath(origin.getData(), destination.getData(), g);
 
-                        for (int i = 0; i < path.length; i++) {
-                                NodeV node = findNode(path[i], g);
-                                if (node == null)
-                                        continue;
+			System.out.print("Ruta Dijkstra: ");
+			for (int i = 0; i < path.length; i++) {
+			    System.out.print(path[i]);
+			    if (i < path.length - 1) System.out.print(" -> ");
+			}
+			System.out.println();
+			
+			for (int i = 0; i < path.length; i++) {
+				NodeV node = findNode(path[i], g);
 
-                                System.out.println(this + " -> " + toCoord(node.getData()));
-                                LogicQueue.add(this, node.getCars());
+				if (node == null)
+					continue;
 
-                                long totalDelay = 0;
-                                RoadList rList = null;
-                                NodeV next = null;
-                                if (i < path.length - 1) {
-                                        next = findNode(path[i + 1], g);
-                                        NodeE edge = findEdge(node, next);
-                                        if (edge != null) {
-                                                totalDelay = (long) edge.getWeight() * VELOCITY_STANDARD;
-                                        }
-                                        rList = selectRoadList(node, next);
-                                }
+				System.out.println(this + " -> " + toCoord(node.getData()));
+				LogicQueue.add(this, node.getCars());
 
-                                if (rList != null && !LogicRoadList.isEmpty(rList)) {
-                                        int steps = LogicRoadList.size(rList) + 1;
-                                        long stepDelay = (steps > 0) ? totalDelay / steps : totalDelay;
-                                        NodeRoad cursor = rList.getFirst();
-                                        while (cursor != null) {
-                                                if (controller != null) {
-                                                        controller.updateCarPosition(cursor.getI(), cursor.getJ());
-                                                }
-                                                try {
-                                                        Thread.sleep(stepDelay);
-                                                } catch (InterruptedException e) {
-                                                        Thread.currentThread().interrupt();
-                                                        return;
-                                                }
-                                                System.out.println(this + " -> (" + cursor.getI() + "," + cursor.getJ() + ")");
-                                                cursor = cursor.getNext();
-                                        }
-                                        if (next != null && controller != null) {
-                                                int nrow = next.getData() / 1000;
-                                                int ncol = next.getData() % 1000;
-                                                controller.updateCarPosition(nrow, ncol);
-                                        }
-                                        try {
-                                                Thread.sleep(stepDelay);
-                                        } catch (InterruptedException e) {
-                                                Thread.currentThread().interrupt();
-                                                return;
-                                        }
-                                } else {
-                                        if (i < path.length - 1 && next != null && controller != null) {
-                                                int nrow = next.getData() / 1000;
-                                                int ncol = next.getData() % 1000;
-                                                controller.updateCarPosition(nrow, ncol);
-                                        }
-                                        try {
-                                                Thread.sleep(totalDelay);
-                                        } catch (InterruptedException e) {
-                                                Thread.currentThread().interrupt();
-                                                return;
-                                        }
-                                }
+				int totalDelay = 0;
+				RoadList rList = null;
+				NodeV next = null;
 
-                                LogicQueue.pop(node.getCars());
-                        }
+				if (i < path.length - 1) {
+					next = findNode(path[i + 1], g);
+					NodeE edge = findEdge(node, next);
+					if (edge != null) {
+						totalDelay = (int) edge.getWeight() * VELOCITY_STANDARD;
+					}
+					rList = selectRoadList(node, next);
+				}
 
-                        System.out.println("Ruta terminada");
+				if (rList != null && !LogicRoadList.isEmpty(rList)) {
+					int steps = LogicRoadList.size(rList) + 1;
+					long stepDelay = (steps > 0) ? totalDelay / steps : totalDelay;
+					NodeRoad cursor = rList.getFirst();
+					while (cursor != null) {
+						if (controller != null) {
+							controller.updateCarPosition(cursor.getI(), cursor.getJ());
+						}
+						try {
+							Thread.sleep(stepDelay);
+						} catch (InterruptedException e) {
+							Thread.currentThread().interrupt();
+							return;
+						}
+						System.out.println(this + " -> (" + cursor.getI() + "," + cursor.getJ() + ")");
+						cursor = cursor.getNext();
+					}
 
-                        NodeV temp = origin;
-                        origin = destination;
-                        destination = temp;
-                }
-        }
+					if (next != null && controller != null) {
+						int nrow = next.getData() / 1000;
+						int ncol = next.getData() % 1000;
+						controller.updateCarPosition(nrow, ncol);
+					}
+					try {
+						Thread.sleep(stepDelay);
+					} catch (InterruptedException e) {
+						Thread.currentThread().interrupt();
+						return;
+					}
+				} else {
 
-        private RoadList selectRoadList(NodeV originV, NodeV destinationV) {
-                int orow = originV.getData() / 1000;
-                int ocol = originV.getData() % 1000;
-                int drow = destinationV.getData() / 1000;
-                int dcol = destinationV.getData() % 1000;
-                if (orow == drow)
-                        return originV.getxRoads();
-                if (ocol == dcol)
-                        return originV.getyRoads();
-                return null;
-        }
+					if (i < path.length - 1 && next != null && controller != null) {
+						int nrow = next.getData() / 1000;
+						int ncol = next.getData() % 1000;
+						controller.updateCarPosition(nrow, ncol);
+					}
+					try {
+						Thread.sleep(totalDelay);
+					} catch (InterruptedException e) {
+						Thread.currentThread().interrupt();
+						return;
+					}
+				}
+
+				LogicQueue.pop(node.getCars());
+			}
+
+			System.out.println("Ruta terminada");
+
+			NodeV temp = origin;
+			origin = destination;
+			destination = temp;
+		}
+	}
+
+	private RoadList selectRoadList(NodeV originV, NodeV destinationV) {
+		int orow = originV.getData() / 1000;
+		int ocol = originV.getData() % 1000;
+		int drow = destinationV.getData() / 1000;
+		int dcol = destinationV.getData() % 1000;
+		if (orow == drow)
+			return originV.getxRoads();
+		if (ocol == dcol)
+			return originV.getyRoads();
+		return null;
+	}
 
 	private NodeV findNode(int data, Graph g) {
 		if (g.getVertices() == null)
